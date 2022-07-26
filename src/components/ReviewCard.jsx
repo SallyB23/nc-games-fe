3629ce94cc2b76219c1fc27bcbec5063cc341271
SallyB23 <sally.bentley.1612@googlemail.com>
2022-07-26 20:06:52 +0100

    link review by id to read review button

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 export default function ReviewCard ({ review }) {
-    const { title, review_img_url } = review
+    const { title, review_img_url, review_id } = review
+    const navigate = useNavigate()
+
+
+    const handleClick = () => {
+          navigate(`../review/${review_id}`)
+    }
+
     return <div className="review-card">
         <img src={review_img_url} alt="review img" />
         <div className="review-selection">
             <h3>{title}</h3>
-            <button>Read Review</button>   
+            <button onClick={handleClick}>Read Review</button>   
         </div>
     </div>
 }
