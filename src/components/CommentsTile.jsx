1818c4e84529a79faa0cd9ea33d1b405b969611b
SallyB23 <sally.bentley.1612@googlemail.com>
2022-07-29 09:01:10 +0100

    add new comment form functionality

--- src/components/CommentsTile.jsx
+++ src/components/CommentsTile.jsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react"
 import { getCommentsForReview } from "../axios"
 import CommentCard from "./CommentCard"
+import NewCommentForm from "./NewCommentForm"
 
 export default function CommentsTile ({ review_id }) {
     const [ commentList, setCommentList ] = useState([])
+    const [ isLoading, setIsLoading ] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         getCommentsForReview(review_id).then(({ data }) => {
             setCommentList(data.comments)
+        setIsLoading(false)
         })
     }, [])
 
+    if (isLoading) return <p>loading...</p>
     return <section className="comments-section">
         <h2>Comments</h2>
-        {commentList.length === 0 ? <p>Be the first to comment on this review</p> : 
-         <>
             {commentList.map(comment => {
             return <CommentCard key={comment.comment_id} comment={comment}/>
         })}
-         </>
-}
+        <NewCommentForm review_id={review_id} setCommentList={setCommentList} commentList={commentList}/>
     </section>
 }
--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -2,25 +2,60 @@ import { useContext, useState } from "react"
 import { postNewComment } from "../axios"
 import { UserContext } from "../contexts/User"
 
-export default function NewCommentForm ({ review_id }) {
+export default function NewCommentForm ({ review_id, setCommentList, commentList }) {
     const { username } = useContext(UserContext)
     const [ newComment, setNewComment ] = useState("")
-
+    const [ isErr, setIsErr ] = useState(false)
+    const [ errMsg, setErrMsg ] = useState("")
+    const [ isUpdating, setIsUpdating ] = useState(false)
     
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(username)
-        postNewComment(review_id, username, newComment)
-        .catch(err => {
-            console.log(err.response)
-        })
+        setIsUpdating(true)
+        let postComment = newComment
+        setNewComment("")
+        if (newComment.length !== 0) {
+            postNewComment(review_id, username, postComment).then(({data}) => {
+                setCommentList((currList) => {
+                    const newList = [...currList]
+                    newList.push(data.comment)
+                    return newList                
+                })
+                setIsUpdating(false)
+            })
+            .catch(err => {
+                setIsErr(true)
+                console.log(err)
+                setIsUpdating(false)
+            })
+        } else {
+            setIsErr(true)
+            setIsUpdating(false)
+            setErrMsg("You have not entered anything!")
+        }
+    }
+
+    const handleOnChange = (e) => {
+        setIsErr(false)
+        setNewComment(e.target.value)
     }
 
     return <section className="new-comment">
         <form onSubmit={handleSubmit}>
-            <label htmlFor="new-comment-body">Leave a comment:</label><br />
-            <textarea onChange={(e) => {setNewComment(e.target.value)}} name="new-comment-body" id="new-comment-body" cols="60" rows="10"></textarea>
-            <button type='submit'>Submit</button>
+            {commentList.length === 0 ? 
+            <label htmlFor="new-comment-body">Be the first to comment on this review</label>
+            :<label htmlFor="new-comment-body">Leave a comment:</label>}
+            <br/><br />
+            {username ? 
+                <> 
+                    <textarea value={newComment}onChange={handleOnChange} name="new-comment-body" id="new-comment-body" cols="60" rows="10"></textarea><br />
+                    <button disabled={isUpdating} type='submit'>Submit</button>
+                </> :
+                <p className="new-comment-logged-out">You need to be logged in to leave a comment</p>
+            }   
+
+            {isErr && <p>{errMsg}</p>}
+            {isUpdating && <p>Please wait...</p>}
         </form>
     </section>
 }
