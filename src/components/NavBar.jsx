c128ccd77a426e5a58decb7789aabebf8ce85c6d
SallyB23 <sally.bentley.1612@googlemail.com>
2022-07-29 11:16:49 +0100

    add sign in form functionality/add sign up form design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,18 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { UserContext } from "../contexts/User"
 import "./NavBar.css"
 
 export default function NavBar() {
+    const { username, setUsername } = useContext(UserContext)
+
     return (
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/reviews">Reviews</Link></li>
-                <li><Link to="">Log out</Link></li>
+                {username ? <li><Link onClick={() => setUsername("")}to="/sign-in">Sign out</Link></li>
+                : <li><Link to="/sign-in">Sign In</Link></li>}
             </ul>
         </nav>
     )
